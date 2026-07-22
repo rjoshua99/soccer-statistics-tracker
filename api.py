e73f7ecb7b4ebd4137780860301f56a356a40511
rjoshua99 <rjoshuaw.42@gmail.com>
2026-07-21 17:59:19 -0700

    Added get_standings, get_scorers, get_matches functions to api.py

---
+++ api.py
@@ -0,0 +1,37 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+API_KEY = os.getenv("FOOTBALL_API_KEY")
+BASE_URL = "https://api.football-data.org/v4"
+
+headers = {"X-Auth-Token": API_KEY}
+
+def get_standings():
+    response = requests.get(f"{BASE_URL}/competitions/PL/standings", headers=headers)
+    response.raise_for_status()
+    return response.json()
+
+def get_scorers():
+    response = requests.get(f"{BASE_URL}/competitions/PL/scorers", headers=headers)
+    response.raise_for_status()
+    return response.json()
+
+def get_matches():
+    response = requests.get(f"{BASE_URL}/competitions/PL/matches", headers=headers)
+    response.raise_for_status()
+    return response.json()
+
+
+
+if __name__ == "__main__":
+    result = get_standings()
+    print(result["standings"][0]["table"][0])
+
+    scorers_result = get_scorers()
+    print(scorers_result["count"])
+
+    matches_result = get_matches()
+    print (matches_result["matches"][0])
+    
